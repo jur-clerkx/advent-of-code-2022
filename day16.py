be8898f24af512ea9288f20670d3651fa72a5683
Jur Clerkx <jur.clerkx@kabisa.nl>
2022-12-22 09:42:48 +0100

    Add partial day 16 part 2

--- day16.py
+++ day16.py
@@ -17,13 +17,21 @@ class Valve:
         return max_pressure_release, effort
 
 
+path_cache = dict()
+
+
 def find_shortest_path(start: Valve, end: Valve, valves: dict[str, Valve]) -> int:
     queue = [(start, 0)]
     visited = set()
 
+    if path_cache.get((start.name, end.name)):
+        return path_cache.get((start.name, end.name))
+
     while queue:
         current_valve, steps = queue.pop(0)
         if current_valve == end:
+            path_cache[(start.name, end.name)] = steps
+            path_cache[(end.name, start.name)] = steps
             return steps
         if current_valve not in visited:
             visited.add(current_valve)
@@ -48,6 +56,38 @@ def get_all_possible_releases(path: list, valves: dict[str, Valve], steps, curre
     return highest_score
 
 
+def get_all_possible_releases_for_two(path1: list, path2: list, valves: dict[str, Valve], steps1, steps2,
+                                      current_score):
+    current1 = path1[-1]
+    current2 = path2[-1]
+    highest_score = current_score
+    # Action for 1
+    for valve in valves:
+        valve = valves[valve]
+        if valve.name not in path1 and valve.name not in path2:
+            score, effort = valve.get_max_pressure_release(current1, valves, steps1)
+            if score > 0 and effort <= steps1:
+                new_path = path1.copy()
+                new_path.append(valve.name)
+                path_score = get_all_possible_releases_for_two(new_path, path2, valves, steps1 - effort, steps2,
+                                                               current_score + score)
+                if path_score > highest_score:
+                    highest_score = path_score
+    # Action for 2
+    for valve in valves:
+        valve = valves[valve]
+        if valve.name not in path1 and valve.name not in path2:
+            score, effort = valve.get_max_pressure_release(current2, valves, steps2)
+            if score > 0 and effort <= steps1:
+                new_path = path2.copy()
+                new_path.append(valve.name)
+                path_score = get_all_possible_releases_for_two(path1, new_path, valves, steps1, steps2 - effort,
+                                                               current_score + score)
+                if path_score > highest_score:
+                    highest_score = path_score
+    return highest_score
+
+
 def parse_input(lines: list[str]) -> dict[str, Valve]:
     result = {}
     for line in lines:
@@ -62,3 +102,5 @@ input_file = open('input/day16.txt', 'r')
 input_lines = [line for line in input_file.read().splitlines()]
 valves = parse_input(input_lines)
 print(get_all_possible_releases(["AA"], valves, 30, 0))
+
+print(get_all_possible_releases_for_two(["AA"], ["AA"], valves, 26, 26, 0))
